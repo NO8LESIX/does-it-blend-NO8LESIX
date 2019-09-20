4cb48debc46cf268f5a8957524d7fcaa9243c9c1
cbcerna <cbcerna@CIS-RDS1>
2019-09-20 14:29:10 -0500

    Version 1.0

---
+++ DoesItBlend/IBlendable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoesItBlend
+{
+    /// <summary>
+    /// Interface for items that an be blended
+    /// </summary>
+    interface IBlendable
+    {
+        /// <summary>
+        /// Blends an object
+        /// </summary>
+        /// <returns>String containing the blended object</returns>
+        string Blend();
+    }
+}
--- DoesItBlend/Program.cs
+++ DoesItBlend/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace DoesItBlend
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<IBlendable> blendable = new List<IBlendable>();
+            blendable.Add(new Strawberry());
+            blendable.Add(new Banana());
+            for(int i = 0; i < 10; i++)
+                blendable.Add(new Mango());
+
+            blendable.Add(new CellPhone());
+            blendable.Add(new IceCubes());
+            Banana b = new Banana();
+            Console.WriteLine(b.Blend());
+            blendable.Add(b);
+            string mess = "";
+            foreach (IBlendable o in blendable)
+            {
+                mess += o.Blend() + ",\n";
+            }
+            Console.WriteLine(mess);
+
+
         }
     }
 }
